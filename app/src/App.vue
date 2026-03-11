055dc26326bc3a50b49df16c0aceeab5131511aa
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-03-11 12:25:56 -0400

    fix: actually use tailwind

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="bg-sky-400">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <style scoped></style>
