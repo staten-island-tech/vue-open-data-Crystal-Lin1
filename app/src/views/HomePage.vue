2771753ed4d03517804fc1d7cbb1e9acbb23f301
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-03-13 12:30:03 -0400

    feat: get api data (wip)

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-
+        <h1>tes</h1>
     </div>
 </template>
 
 <script setup lang="ts">
-
+async function getAllData() {
+    const url = 'https://data.cityofnewyork.us/api/v3/views/43nn-pn8j/query.json'
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+    }
+getAllData()
 </script>
 
 <style scoped>
