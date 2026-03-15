80594fdc4824a7438926dae99a04e01bce5dee6b
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-03-14 22:03:00 -0400

    feat: home page header

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -1,13 +1,19 @@
 <template>
-    <header class="bg-red-200 py-5">
-        <h1 class="font-bold text-xl">Sketchy Restaurant Detector</h1>
-        <h2 class="italic">health inspection scores</h2>
+    <header class="bg-red-200 py-5 flex justify-between shadow">
+        <div class="ml-2">
+            <h1 class="font-bold text-xl">Sketchy Restaurant Detector</h1>
+            <h2 class="italic">health inspection scores</h2>
+        </div>
+        <img :src='utensilsImg' class="w-[4%]">
     </header>
+    
 </template>
 
 <script setup lang="ts">
+import utensilsImg from '@/assets/utensils.png'
 async function getAllData(): Promise<void> {
     const url = 'https://data.cityofnewyork.us/api/v3/views/43nn-pn8j/query.json'
+    //TODO: get key (DONT COMMIT ITTTT!!)
     try {
         const response = await fetch(url)
         const data = await response.json()
