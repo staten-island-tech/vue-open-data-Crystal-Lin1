f58c08af7ac3e3d3bd350a9a702e8e735ed29b28
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-03-18 12:32:10 -0400

    feat: boro data css (wip)

--- app/src/components/BoroughData.vue
+++ app/src/components/BoroughData.vue
@@ -1,16 +1,26 @@
 <template>
-    <div class="shadow w-[50%] p-10 m-auto">
-        <form>
-            <input id='statenIsland' name='boro' value='statenIsland' type="radio">
-            <label for="statenIsland">Staten Island</label>
-            <input id='manhattan' name='boro' value='manhattan' type="radio">
-            <label for="manhattan">Manhattan</label>
-            <input id='brooklyn' name='boro' value='brooklyn' type="radio">
-            <label for="brooklyn">Brooklyn</label>
-            <input id='bronx' name='boro' value='bronx' type="radio">
-            <label for="bronx">Bronx</label>
-            <input id='queens' name='boro' value='queens' type="radio">
-            <label for="queens">Queens</label>
+    <div class="shadow w-[50%] p-10 m-auto flex justify-center">
+        <form @submit.prevent="" >
+            <div class="flex">
+                <input class='mr-2'id='statenIsland' name='boro' value='statenIsland' type="checkbox">
+                <label for="statenIsland">Staten Island</label>
+            </div>
+            <div class="">
+                <input class='mr-2' id='manhattan' name='boro' value='manhattan' type="checkbox">
+                <label for="manhattan">Manhattan</label>
+            </div>
+            <div class="">
+                <input class='mr-2' id='brooklyn' name='boro' value='brooklyn' type="checkbox">
+                <label for="brooklyn">Brooklyn</label>
+            </div>
+            <div class="">
+                <input class='mr-2' id='bronx' name='boro' value='bronx' type="checkbox">
+                <label for="bronx">Bronx</label>
+            </div>
+            <div class="">
+                <input class='mr-2'id='queens' name='boro' value='queens' type="checkbox">
+                <label for="queens">Queens</label>
+            </div>
         </form>
         
     </div>
